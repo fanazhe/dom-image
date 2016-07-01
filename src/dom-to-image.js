1b6d9f70a3aa7d682e7cf3c17773bc9aa9a550d6
fanazhe <fanazhe@gmail.com>
2016-07-02 06:23:55 +0800

    Merge multiple string additions into one to reduce memory cost when inlining huge images or web fonts

--- src/dom-to-image.js
+++ src/dom-to-image.js
@@ -300,14 +300,9 @@
             })
             .then(util.escapeXhtml)
             .then(function (xhtml) {
-                return '<foreignObject x="0" y="0" width="100%" height="100%">' + xhtml + '</foreignObject>';
-            })
-            .then(function (foreignObject) {
-                return '<svg xmlns="http://www.w3.org/2000/svg" width="' + width + '" height="' + height + '">' +
-                    foreignObject + '</svg>';
-            })
-            .then(function (svg) {
-                return 'data:image/svg+xml;charset=utf-8,' + svg;
+                return 'data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="' + width +
+                    '" height="' + height + '"><foreignObject x="0" y="0" width="100%" height="100%">' + xhtml +
+                    '</foreignObject></svg>'
             });
     }
 
